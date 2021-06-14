0214af018d74d66be12bd737176927f117286c6b
Safal Adhikari <safal.adhikari1@outlook.com>
2021-06-14 14:43:04 -0700

    adds Employee Service for the project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Employee } from './employee';
+import { EmployeeService } from './employee.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'employeemanagerapp';
+  public employees: Employee[];
+
+  constructor(private employeeService: EmployeeService) { }
 }
