c0dfc9422db8bc412344725334ad3f0b2391d8a9
Safal Adhikari <safal.adhikari1@outlook.com>
2021-06-27 16:33:34 -0700

    finishes testing the modal logic and UI template

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,7 +10,7 @@ import { EmployeeService } from './employee.service';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit{
-  public employees: Employee[] = [];
+  public employees: Employee[];
 
   constructor(private employeeService: EmployeeService) { }
 
@@ -29,7 +29,25 @@ export class AppComponent implements OnInit{
     );
   }
 
+  public onOpenModal(employee: Employee, mode: string): void {
+    const container = document.getElementById('main-container');
+    const button = document.createElement('button');
+    button.type = 'button';
+    button.style.display = 'none';
+    button.setAttribute('data-toggle', 'modal');
+    if (mode === 'add') {
+      button.setAttribute('data-target', '#addEmployeeModal');
+    }
 
+    if (mode === 'edit') {
+      button.setAttribute('data-target', '#updateEmployeeModal');
+    }
 
+    if (mode === 'delete') {
+      button.setAttribute('data-target', '#deleteEmployeeModal');
+    }
 
+    container?.appendChild(button);
+    button.click();
+  }
 }
